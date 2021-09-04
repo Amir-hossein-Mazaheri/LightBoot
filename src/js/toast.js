0620b09e3c2559cbf242b1c9ae98119655dd153f
Amirhossein <uplayggboy@gmail.com>
2021-09-04 23:33:20 +0430

    toast function added

--- src/js/toast.js
+++ src/js/toast.js
@@ -1,14 +1,14 @@
-const toastToggle = document.querySelectorAll('[toast-Toggle]');
+const toastToggle = document.querySelectorAll('[toast-toggle]');
 let toastIsClicked = false;
 
 toastToggle.forEach(value => {
     document.querySelector("body").classList.add('overflow-x-h');
     const toast = document.querySelector(value.getAttribute('toast-Toggle'));
     const toastWidth = toast.offsetWidth;
+    toast.style.opacity = '0';
 
     if (toast.classList.contains('toast-bottom-right') || toast.classList.contains('toast-top-right')) {
         toast.style.transform = `translateX(calc(${toastWidth}px + 2rem))`;
-        toast.style.opacity = '0';
         value.addEventListener('click', () => {
             if (toastIsClicked == false) {
                 toast.style.transform = 'none';
@@ -20,11 +20,8 @@ toastToggle.forEach(value => {
                 toastIsClicked = false;
             }
         })
-    }
-
-    if (toast.classList.contains('toast-bottom-left') || toast.classList.contains('toast-top-left')) {
+    } else {
         toast.style.transform = `translateX(calc((-${toastWidth}px - 2rem)))`;
-        toast.style.opacity = '0';
         value.addEventListener('click', () => {
             if (toastIsClicked == false) {
                 toast.style.transform = 'none';
@@ -39,3 +36,22 @@ toastToggle.forEach(value => {
     }
 
 });
+
+const toast = function (toast_type, toast_pos, toast_content, toast_interval = 150) {
+    const toastElm = document.createElement('div');
+    toastElm.classList.add('toast-' + toast_type);
+    toastElm.classList.add('toast-' + toast_pos);
+    toastElm.innerHTML = toast_content;
+    document.querySelector('body').prepend(toastElm);
+
+    setInterval(() => {
+        if (toast_pos == 'bottom-right' || toast_pos == 'top-right') {
+            toastElm.style.transform = `translateX(calc(${toastElm.offsetWidth}px + 2rem))`;
+            toast.style.opacity = '0';
+        } else {
+            toastElm.style.transform = `translateX(calc(${-toastElm.offsetWidth}px - 2rem))`;
+            toast.style.opacity = '0';
+        }
+
+    }, toast_interval)
+}
