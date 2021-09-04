8219d5ea1b0abec3fb54e7ebc92c3ac68fb910a8
Amirhossein <uplayggboy@gmail.com>
2021-09-04 22:56:34 +0430

    toast fully added and debuged

--- src/js/toast.js
+++ src/js/toast.js
@@ -1,19 +1,22 @@
-const toastOpen = document.querySelectorAll('[toast-open]');
+const toastToggle = document.querySelectorAll('[toast-Toggle]');
 let toastIsClicked = false;
 
-toastOpen.forEach(value => {
+toastToggle.forEach(value => {
     document.querySelector("body").classList.add('overflow-x-h');
-    const toast = document.querySelector(value.getAttribute('toast-open'));
+    const toast = document.querySelector(value.getAttribute('toast-Toggle'));
     const toastWidth = toast.offsetWidth;
 
     if (toast.classList.contains('toast-bottom-right') || toast.classList.contains('toast-top-right')) {
         toast.style.transform = `translateX(calc(${toastWidth}px + 2rem))`;
+        toast.style.opacity = '0';
         value.addEventListener('click', () => {
             if (toastIsClicked == false) {
                 toast.style.transform = 'none';
+                toast.style.opacity = '1';
                 toastIsClicked = true;
             } else {
                 toast.style.transform = `translateX(calc(${toastWidth}px + 2rem))`;
+                toast.style.opacity = '0';
                 toastIsClicked = false;
             }
         })
@@ -21,32 +24,18 @@ toastOpen.forEach(value => {
 
     if (toast.classList.contains('toast-bottom-left') || toast.classList.contains('toast-top-left')) {
         toast.style.transform = `translateX(calc((-${toastWidth}px - 2rem)))`;
+        toast.style.opacity = '0';
         value.addEventListener('click', () => {
             if (toastIsClicked == false) {
                 toast.style.transform = 'none';
+                toast.style.opacity = '1';
                 toastIsClicked = true;
             } else {
                 toast.style.transform = `translateX(calc((-${toastWidth}px - 2rem)))`;
+                toast.style.opacity = '0';
                 toastIsClicked = false;
             }
         })
     }
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
