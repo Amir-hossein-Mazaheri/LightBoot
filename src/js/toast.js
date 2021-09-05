e57f354eac52d0e5f4070051d2e1df6e67ae222d
Amirhossein <uplayggboy@gmail.com>
2021-09-05 15:34:39 +0430

    added badges and fix toast

--- src/js/toast.js
+++ src/js/toast.js
@@ -37,21 +37,26 @@ toastToggle.forEach(value => {
 
 });
 
-const toast = function (toast_type, toast_pos, toast_content, toast_interval = 150) {
+const toast = function (toast_type, toast_pos, toast_content, toast_close_button = false, toast_exit_interval = 150) {
     const toastElm = document.createElement('div');
     toastElm.classList.add('toast-' + toast_type);
     toastElm.classList.add('toast-' + toast_pos);
     toastElm.innerHTML = toast_content;
+    if(toast_close_button == true) {
+        const timeToast = new Date();
+        const milSec = timeToast.getMilliseconds();
+        let uniqueId = toastElm.setAttribute('id', 'tsc' + milSec );
+        toastElm.innerHTML += `<div class="close-button txt-white" toast-toggle="#tsc${milSec}"></div>`;
+    }
     document.querySelector('body').prepend(toastElm);
 
-    setInterval(() => {
-        if (toast_pos == 'bottom-right' || toast_pos == 'top-right') {
-            toastElm.style.transform = `translateX(calc(${toastElm.offsetWidth}px + 2rem))`;
-            toast.style.opacity = '0';
-        } else {
-            toastElm.style.transform = `translateX(calc(${-toastElm.offsetWidth}px - 2rem))`;
-            toast.style.opacity = '0';
-        }
-
-    }, toast_interval)
+    if (toast_exit_interval != 0) {
+        setInterval(() => {
+            if (toast_pos == 'bottom-right' || toast_pos == 'top-right') {
+                toastElm.style.transform = `translateX(calc(${toastElm.offsetWidth}px + 2rem))`;
+            } else {
+                toastElm.style.transform = `translateX(calc(${-toastElm.offsetWidth}px - 2rem))`;
+            }
+        }, toast_exit_interval);
+    }
 }
