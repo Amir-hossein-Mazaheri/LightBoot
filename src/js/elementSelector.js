be539d3c5c2229a36757eb6246b097a3f3e4166c
Amirhossein <uplayggboy@gmail.com>
2021-09-05 19:57:50 +0430

    added elementSelector and modal center and modal bottom

---
+++ src/js/elementSelector.js
@@ -0,0 +1,19 @@
+function _(element) {
+    return document.querySelector(element);
+}
+
+function __(element) {
+    return document.querySelectorAll(element);
+}
+
+function _i(element_id) {
+    return document.getElementById(element_id);
+}
+
+function _c(element_class) {
+    return document.getElementsByClassName(element_id);
+}
+
+function _t(tag) {
+    return document.getElementsByTagName(tag);
+}
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,29 +1,25 @@
-function _(element) {
-    return document.querySelector(element);
-}
-
-function __(element) {
-    return document.querySelectorAll(element);
-}
-
 const modalToggler = __('[modal-open]');
 const modalFilter = document.createElement('div');
 modalFilter.classList.add('modal-filter');
 _('body').insertBefore(modalFilter, _('body').childNodes[1]);
 
 modalToggler.forEach(value => {
-    let modalIsClicked = false;
     const modal = _(value.getAttribute('modal-open'));
     const modalCloseBtn = _(value.getAttribute('modal-open') + ' .modal-close');
 
     function modalOpen() {
         modal.style.opacity = '1';
-        modal.style.top = '15%';
+        if (modal.classList.contains('modal-center')) {
+            modal.style.top = '50%';
+        } else if (modal.classList.contains('modal-bottom')) {
+            modal.style.top = '85%';
+        } else {
+            modal.style.top = '15%';
+        }
         modal.style.zIndex = '1800';
         modalFilter.style.opacity = '1';
         modalFilter.style.zIndex = '1700';
         modalCloseBtn.style.display = 'block';
-        modalIsClicked = true;
     }
 
     function modalClose() {
@@ -33,26 +29,17 @@ modalToggler.forEach(value => {
         modalFilter.style.opacity = '0';
         modalFilter.style.zIndex = '-1';
         modalCloseBtn.style.display = 'none';
-        modalIsClicked = false;
     }
 
     value.addEventListener('click', () => {
-        if (modalIsClicked == false) {
-            modalOpen();
-        } else {
-            modalClose();
-        }
+        modalOpen();
     })
 
     modalCloseBtn.addEventListener('click', () => {
-        if (modalIsClicked == true) {
-            modalClose();
-        }
+        modalClose();
     });
 
     modalFilter.addEventListener('click', () => {
-        if (modalIsClicked == true) {
-            modalClose();
-        }
+        modalClose();
     });
 })
