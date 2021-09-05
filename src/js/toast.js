0d35f12df613f7887b1001ad255d1e2799d22389
Amirhossein <uplayggboy@gmail.com>
2021-09-05 17:18:53 +0430

    some fixes to toast and badges

--- src/js/toast.js
+++ src/js/toast.js
@@ -3,7 +3,7 @@ let toastIsClicked = false;
 
 toastToggle.forEach(value => {
     document.querySelector("body").classList.add('overflow-x-h');
-    const toast = document.querySelector(value.getAttribute('toast-Toggle'));
+    const toast = document.querySelector(value.getAttribute('toast-toggle'));
     const toastWidth = toast.offsetWidth;
     toast.style.opacity = '0';
 
@@ -37,26 +37,47 @@ toastToggle.forEach(value => {
 
 });
 
-const toast = function (toast_type, toast_pos, toast_content, toast_close_button = false, toast_exit_interval = 150) {
+const toast = function (toast_type = 'danger', toast_pos = 'bottom-left', toast_content = 'NOTHING ENTERED AS A CONTENT FOR TOAST!!!!', toast_close_button = false, toast_entrance_interval = 200, toast_exit_interval = 600) {
     const toastElm = document.createElement('div');
     toastElm.classList.add('toast-' + toast_type);
     toastElm.classList.add('toast-' + toast_pos);
+    toastElm.style.opacity = '0';
     toastElm.innerHTML = toast_content;
-    if(toast_close_button == true) {
-        const timeToast = new Date();
-        const milSec = timeToast.getMilliseconds();
-        let uniqueId = toastElm.setAttribute('id', 'tsc' + milSec );
-        toastElm.innerHTML += `<div class="close-button txt-white" toast-toggle="#tsc${milSec}"></div>`;
-    }
-    document.querySelector('body').prepend(toastElm);
 
-    if (toast_exit_interval != 0) {
-        setInterval(() => {
-            if (toast_pos == 'bottom-right' || toast_pos == 'top-right') {
-                toastElm.style.transform = `translateX(calc(${toastElm.offsetWidth}px + 2rem))`;
-            } else {
-                toastElm.style.transform = `translateX(calc(${-toastElm.offsetWidth}px - 2rem))`;
-            }
-        }, toast_exit_interval);
+    function toastEffect() {
+        if (toast_pos == 'bottom-right' || toast_pos == 'top-right') {
+            toastElm.style.transform = `translateX(calc(${toastElm.offsetWidth}px + 2rem))`;
+        } else {
+            toastElm.style.transform = `translateX(calc(${-toastElm.offsetWidth}px - 2rem))`;
+        }
     }
+
+    if (toast_close_button == true) {
+        toastCloseBtn = document.createElement('div');
+        toastCloseBtn.classList.add('close-button');
+        toastElm.append(toastCloseBtn);
+        toastCloseBtn.addEventListener('click', () => {
+            toastElm.style.opacity = '0';
+            toastEffect()
+        });
+    }
+
+    document.querySelector('body').prepend(toastElm);
+    toastEffect()
+
+    setTimeout(() => {
+        toastElm.style.opacity = '1';
+        if (toast_pos == 'bottom-right' || toast_pos == 'top-right') {
+            toastElm.style.transform = `none`;
+        } else {
+            toastElm.style.transform = `none`;
+        }
+
+        if (toast_exit_interval != 0) {
+            setTimeout(() => {
+                toastElm.style.opacity = '0';
+                toastEffect()
+            }, toast_exit_interval + 700);
+        }
+    }, toast_entrance_interval)
 }
