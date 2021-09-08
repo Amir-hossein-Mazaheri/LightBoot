066c9cc5ab53b99d22dccd289f76e6ea114679c2
Amirhossein <uplayggboy@gmail.com>
2021-09-08 14:48:00 +0430

    formated tab.js

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,44 +1,37 @@
-const tabItems = document.querySelectorAll(".tab-item");
-const tabs = document.querySelectorAll(".tab");
+// imports element selector functions
+import {__, _, _i} from './elementSelector.js';
+
+const tabItems = __(".tab-item");
+const tabs = __(".tab");
 const countTabItems = tabItems.length;
-const tabContents = document.querySelectorAll(".tab-content");
+const tabContents = __(".tab-content");
 let tabBodyAcc = 0;
 let tabBodyLength;
 
 const tab = (function () {
     for (let counter = 0; counter < countTabItems; counter++) {
         tabItems[counter].setAttribute("id", "tb" + (counter + 1));
-        let tabCont = document.querySelector(
-            document
-                .getElementById("tb" + (counter + 1))
-                .getAttribute("data-tab-content")
+        let tabCont = _(
+            _i("tb" + (counter + 1)).getAttribute("data-tab-content")
         );
         tabBodyLength = Number(
-            document.querySelectorAll(
-                document
-                    .getElementById("tb" + (counter + 1))
-                    .getAttribute("data-tab-body") + "> *"
-            ).length
+            __(_i("tb" + (counter + 1)).getAttribute("data-tab-body") + "> *").length
         );
         if (counter < tabs.length) {
             tabBodyAcc += tabBodyLength;
             console.log("total is:" + tabBodyAcc);
         }
         var county;
-        document
-            .querySelector("#tb" + (counter + 1))
-            .addEventListener("click", function () {
-                document
-                    .querySelector("#tb" + (counter + 1))
-                    .classList.toggle("active");
-                tabCont.classList.toggle("active");
-                for (county = 0; county < tabBodyAcc; county++) {
-                    if (county == counter) {
-                        continue;
-                    }
-                    tabItems[county].classList.remove("active");
-                    tabContents[county].classList.remove("active");
+        _("#tb" + (counter + 1)).addEventListener("click", function () {
+            _("#tb" + (counter + 1)).classList.toggle("active");
+            tabCont.classList.toggle("active");
+            for (county = 0; county < tabBodyAcc; county++) {
+                if (county == counter) {
+                    continue;
                 }
-            });
+                tabItems[county].classList.remove("active");
+                tabContents[county].classList.remove("active");
+            }
+        });
     }
 })();
